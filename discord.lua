59a17eb1f740d4ec94a92b2341d5c39bbb49c04b
JoeStarMama <ddashafieiadjbish2@glow.sch.uk>
2021-09-19 19:22:55 +0100

    Files

--- discord.lua
+++ discord.lua
@@ -1,13 +1,7 @@
 Citizen.CreateThread(function()
-   -- while true do
-        SetDiscordAppId('859065967715942400')
-        SetDiscordRichPresenceAsset('hoperp') 
-        SetDiscordRichPresenceAssetText('discord.gg/Tmfa4Uz')
-        SetDiscordRichPresenceAction(0, "Connect with FiveM", "https://hoperp.co.uk/server-connect/") 
-        SetDiscordRichPresenceAction(1, "Discord", "https://discord.com/invite/Tmfa4Uz")
-
-        --Puts the number of players into the status
-    --    Citizen.Wait(1000)
-        --SetRichPresence(GetNumberOfPlayers() ..  " Player's Online")
-   -- end
+        SetDiscordAppId('12135243423243') -- Put app id from https://discord.com/developers/applications
+        SetDiscordRichPresenceAsset('img') -- img from Discord App made in portal
+        SetDiscordRichPresenceAssetText('discord.gg/dwadwadwa')
+        SetDiscordRichPresenceAction(0, "Connect with FiveM", "https://example.com/") 
+        SetDiscordRichPresenceAction(1, "Discord", "https://discord.com/invite/dwaadw")
 end)
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,11 +2,9 @@ fx_version 'cerulean'
 games {'gta5'}
 
 author 'Joe is a Rogan'
-description 'Kind of working RPC'
+description 'JoeRPC'
 version '1.0.0'
 
 client_scripts {
     'discord.lua'
 }
-
-client_script '@hrp_progbar/36958.lua'
