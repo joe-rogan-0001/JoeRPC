29b2a19d989e5004fbb01d4581dcfabed8fd9322
Joe Rogan <76793890+joe-rogan-0001@users.noreply.github.com>
2021-10-15 20:06:03 +0100

    Update discord.lua

--- discord.lua
+++ discord.lua
@@ -1,7 +1,7 @@
 Citizen.CreateThread(function()
         SetDiscordAppId('12135243423243') -- Put app id from https://discord.com/developers/applications
         SetDiscordRichPresenceAsset('img') -- img from Discord App made in portal
-        SetDiscordRichPresenceAssetText('discord.gg/dwadwadwa')
+        SetDiscordRichPresenceAssetText('discord.gg/example')
         SetDiscordRichPresenceAction(0, "Connect with FiveM", "https://example.com/") 
         SetDiscordRichPresenceAction(1, "Discord", "https://discord.com/invite/dwaadw")
 end)
